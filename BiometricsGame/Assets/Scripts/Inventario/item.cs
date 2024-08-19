a8a2a0a419db98248f2eec4167241db46596297f
willyshm <sanchez.willy129@gmail.com>
2024-08-19 09:32:19 -0500

    ConfiguracionCamara
    En este punto lo que hicimos fue configurar la cámara y sus cosas básicas.

--- BiometricsGame/Assets/Scripts/Inventario/item.cs
+++ BiometricsGame/Assets/Scripts/Inventario/item.cs
@@ -6,6 +6,6 @@ using static UnityEditor.Progress;
 public class item : MonoBehaviour
 {
     public string itemName = "New Item";  // Nombre del objeto
-    public Sprite itemIcon = null;  // Ícono del objeto
+    public Sprite itemIcon;  // Ícono del objeto
 
 }
